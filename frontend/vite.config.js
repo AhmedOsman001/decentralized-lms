5aa3fff0cee408aeb44268f167b48336b78b25bb
root <root@LAPTOP-NJH4QDOB.localdomain>
2025-07-24 22:50:38 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/decentralized-lms/', // 👈 MUST match your repo name
   plugins: [react()],
   build: {
     outDir: 'dist',
